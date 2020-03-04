e0eef1babce92ff9b51234fbba81b29d1cfa7508
Drake Nguyen <drakeknguyen@yahoo.com>
2020-03-04 00:07:44 -0800

    Updates

---
+++ precache-manifest.5bf24c195bf92fdef7ce1cd56ce97005.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1c53ada2a747492cd3bb64d0873326",
+    "url": "/weatherforecast/index.html"
+  },
+  {
+    "revision": "17bd059763b51398ec3b",
+    "url": "/weatherforecast/static/css/main.449542e6.chunk.css"
+  },
+  {
+    "revision": "bf19dbb4ba2c7d2c2687",
+    "url": "/weatherforecast/static/js/2.289e851d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weatherforecast/static/js/2.289e851d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17bd059763b51398ec3b",
+    "url": "/weatherforecast/static/js/main.a16c6b80.chunk.js"
+  },
+  {
+    "revision": "3685ad5ba84dee8f15d5",
+    "url": "/weatherforecast/static/js/runtime-main.889f3e3b.js"
+  },
+  {
+    "revision": "b479a9605b65f815f0d295817edf53e4",
+    "url": "/weatherforecast/static/media/cloudy.b479a960.jpg"
+  },
+  {
+    "revision": "94c0025741db25bccc533e0052e4c071",
+    "url": "/weatherforecast/static/media/newyork.94c00257.jpg"
+  },
+  {
+    "revision": "f9ffc495798edf949cdb1dc2d9000236",
+    "url": "/weatherforecast/static/media/rain.f9ffc495.jpg"
+  },
+  {
+    "revision": "c0e71bada3dde940ba3965c9085d3558",
+    "url": "/weatherforecast/static/media/snow.c0e71bad.jpg"
+  },
+  {
+    "revision": "4d9bbfab6c9f32ceb16847d8d3325112",
+    "url": "/weatherforecast/static/media/sunny.4d9bbfab.jpg"
+  },
+  {
+    "revision": "1e2351c0e45dc3455071ccd2424e2b6d",
+    "url": "/weatherforecast/static/media/thunderstorm.1e2351c0.jpg"
+  }
+]);
