cb915e4a75c29b4b59d6b19102da471b96f6f48a
Drake Nguyen <drakeknguyen@yahoo.com>
2020-03-10 13:02:04 -0700

    added link to footer

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,7 +16,8 @@ class SearchBar extends React.Component {
           <div className="field">
             <label>Search City</label>
             <input placeholder="Los Angeles, New York, Paris, London, Cairo, Tokyo, etc." type="text" value={this.state.term} onChange={ event => this.setState({term: event.target.value})} ></input>
-          <p className="copyright">&copy; Designed by Drake Nguyen 2020. Powered by <a class="metaweather" href="https://www.metaweather.com/"> MetaWeather </a> API.</p>
+            <p className="copyright">&copy; Designed by <a class="link" href="https://drakenguyen4000.github.io/portfolio/">Drake
+            Nguyen 2020</a>. Powered by <a class="link" href="https://www.metaweather.com/"> MetaWeather </a> API.</p>
           </div>
         </form>
       </div>
